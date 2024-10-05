bc9a5edfa9b689226fd813988f9d0c993beb5829
aabr2612 <aabr2612@gmail.com>
2024-10-05 12:11:16 +0500

    Data scraping functionality fix

--- frontend.py
+++ frontend.py
@@ -14,15 +14,18 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Project-1 Scraping and Data sorting") # setting the window title
         
         # setting the geometry of the form
-        win_width = 900 # inital width of the form
+        win_width = 800 # inital width of the form
         win_height = 690 # initial height of the form
         screen = QApplication.primaryScreen() # getting the size of the screen
         init_x_position = (screen.geometry().width() - win_width) // 2 # setting the initial start position for the x-axis to ensure that it is in mid
         init_y_position = 30 # setting the initial start position for the y-axis
-        self.setGeometry(init_x_position, init_y_position, win_width, win_height) # setting the initial size and axis of the for
+        self.setGeometry(init_x_position, init_y_position, win_width, win_height) # setting the initial size and axis of the window form
+        
+        # setting the minimum size for the screen
+        self.setMinimumSize(win_width,win_height)
         
         # setting some stylesheet for the window
-        self.setStyleSheet("background-color: grey;")
+        self.setStyleSheet("background-color: grey; font-family: Times New Roman;")
         
         # calling an initalizer function to add objects to the form
         self.initUI()
@@ -39,18 +42,24 @@ class MainWindow(QMainWindow):
         # ------------------------------------- Title layout -------------------------------------
 
         title_label =  QLabel("Project1 - Sorting Algorithms",self) # setting the label text
-        title_label.setFont(QFont("Times New Romans",30)) # setting the text font and size
         title_label.setFixedHeight(80) # fixing the height of the label
         title_label.setAlignment(Qt.AlignCenter) # fixing the alignment of the label
-        title_label.setStyleSheet("color: black;background-color:white;font-weight: bold;") # adding a stylesheet for the label
+        title_label.setStyleSheet("color: black; background-color: white; font-weight: bold; font-size: 30px") # adding a stylesheet for the label
         
         title_container = QVBoxLayout() # title container to hold the label
         title_container.addWidget(title_label) # adding label to the layout
+
+        # ------------------------------------- Scraping URL input -------------------------------------
+        
+        data_scrap_url = QLineEdit()
+        data_scrap_url.setPlaceholderText("Enter a valid URL to scrap data")
+        data_scrap_url.setStyleSheet("color: black; background-color: white; font-size: 14px;")
+        data_scrap_url.setFixedWidth(400)
         
         # ------------------------------------- Scraping buttons layout -------------------------------------
 
         # common stylesheet for the buttons in the UI
-        buttons_stylesheet = "color:black; font-weight:bold; font-size: 11;"
+        buttons_stylesheet = "color:black; font-weight:bold; font-size: 14px;"
         
         # start button
         start_scrap_btn = QPushButton("Start")
@@ -72,17 +81,53 @@ class MainWindow(QMainWindow):
         stop_scrap_btn.setStyleSheet(buttons_stylesheet+"background-color: red;")
         # stop_scrap_btn.clicked.connect(self.stop_scrap_data)
         
-        # a buttons container to hold the buttons in the window
-        buttons_container = QHBoxLayout()
-        buttons_container.addWidget(start_scrap_btn)
-        buttons_container.addWidget(pause_scrap_btn)
-        buttons_container.addWidget(resume_scrap_btn)
-        buttons_container.addWidget(stop_scrap_btn)
+        # ------------------------------------- Setting up the URL and buttons in layout -------------------------------------
+        
+        # a container to hold the url input, progress bar and buttons in the window
+        scrapping_functionality_container = QHBoxLayout()
+        scrapping_functionality_container.addStretch(1)
+        scrapping_functionality_container.addWidget(data_scrap_url)
+        scrapping_functionality_container.addWidget(start_scrap_btn)
+        scrapping_functionality_container.addWidget(pause_scrap_btn)
+        scrapping_functionality_container.addWidget(resume_scrap_btn)
+        scrapping_functionality_container.addWidget(stop_scrap_btn)
+        scrapping_functionality_container.addStretch(1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         # ------------------------------------- Adding layouts to main layout -------------------------------------
         
         main_layout.addLayout(title_container)
-        main_layout.addLayout(buttons_container)
+        main_layout.addLayout(scrapping_functionality_container)
         
         
     
